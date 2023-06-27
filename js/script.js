289b1a737335f5601aaf5f9f84b9afb6acd812ab
AstghikMeltonyan <astghikmeltonyan@gmail.com>
2023-06-27 16:45:31 +0400

    add some news

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// const galleryBtn = document.querySelector('.gallery-btn--active')
+// const galleryModal = document.querySelector('.gallery-window');
+// const modalCloseBtn = document.querySelector('.close-btn')
+
+const avocadoBtn = document.querySelector('.avocado-btn')
+
+const avocadoWindow = document.querySelector('.avocado-window');
+
+const blogPage = document.querySelector('.blog-page')
+
+const avocadoCloseBtn = document.querySelector('.close-btn')
+
+avocadoBtn.addEventListener('click', () => {
+  avocadoWindow.classList.add('avocado-window--active');
+  blogPage.classList.add('blog-page--inactive')
+  console.log(555);
+})
+
+avocadoCloseBtn.addEventListener('click', () => {
+  avocadoWindow.classList.remove('avocado-window--active')
+  blogPage.classList.remove('blog-page--inactive')
+  console.log(555);
+})
+
