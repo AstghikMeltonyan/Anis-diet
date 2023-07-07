fffdf88b66308f5914d6296267e91fc9f9fc36d3
AstghikMeltonyan <astghikmeltonyan@gmail.com>
2023-07-07 12:58:13 +0400

    add some media

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const burgerBtn = document.querySelector('.button-burger');
-const navList = document.querySelector('.nav-list')
+const burgerBtn = document.querySelector(".button-burger");
+const navList = document.querySelector(".nav-list");
 
-burgerBtn.addEventListener('click', function () {
-  burgerBtn.classList.toggle('change');
-  navList.classList.toggle('active');
-})
+burgerBtn.addEventListener("click", function () {
+  burgerBtn.classList.toggle("change");
+  navList.classList.toggle("active");
+});
+
